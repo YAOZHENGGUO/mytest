b14af15ef0c0f7cb107775220e20beefaf3c6c8e
YAOZHENGGUO <HAPPYANDYZG@163.com>
2021-09-18 11:26:00 +0800

    excel2

--- python/day7【mysql工具类与excel读取】/代码/day7.py
+++ python/day7【mysql工具类与excel读取】/代码/day7.py
@@ -3,7 +3,23 @@ import xlwt
 wb = xlrd.open_workbook(r'../\\数据\\2020年每个月的销售情况.xls')
 
 
-# 月销售额
+# 月销售统计
+def cloth_per_moth(x):
+    sheet = wb.sheet_by_name("{}月".format(x))
+    rows = sheet.nrows
+    tj = {}
+    for i in range(1, rows):
+        data = sheet.row_values(i)
+        s = data[2]*data[4]
+        if data[1] in tj:
+            tj[data[1]] += round(s)
+        else:
+            tj[data[1]] = round(s)
+    print("{}月份销售：{}".format(x, tj))
+    return tj
+
+
+# 月销售总额
 def moth(x):
     sheet = wb.sheet_by_name("{}月".format(x))
     rows = sheet.nrows
@@ -68,7 +84,7 @@ def min_sale(a):
     print('销售最少的是{}，为{}件'.format(i, a[i]))
 
 
-# 销售（件数）占比
+# 销售占比
 def per_sale(a):
     per = {}
     su = sum(a.values())
@@ -81,15 +97,72 @@ def per_sale(a):
 
 
 # 执行
-for q in range(12):
-    q += 1
-    a = '{}'.format(q) + '月'
-    print(q, '月：', mother_sale(q))
+# 全年销售总额
 mother = 0
 for i in range(1, 13):
     mother += moth(i)
 print('年销售总额：', round(mother, 1))
+# 每件衣服的销售（件数）占比
 y = year_sale()
+print('每件衣服的销售（件数）占比')
 per_sale(y)
+# 每件衣服的月销售占比
+print('每种衣服每月销售多少件')
+for q in range(12):
+    q += 1
+    print(q, '月：')
+    per_sale(mother_sale(q))
+# 每件衣服的销售额占比
+for m1 in range(1, 13):
+    per_sale(cloth_per_moth(m1))
+# 最畅销的衣服
+print('全年最畅销的衣服')
 max_sale(y)
+# 每个季度最畅销的衣服
+first_quartely = {}
+mo1 = {}
+for i in [2, 3, 4]:
+    mo1[i] = mother_sale(i)
+    for k, v in mo1[i].items():
+        if k in first_quartely:
+            first_quartely[k] += v
+        else:
+            first_quartely[k] = v
+print('第一季度')
+max_sale(first_quartely)
+second_quartely = {}
+mo2 = {}
+for i in [5, 6, 7]:
+    mo2[i] = mother_sale(i)
+    for k, v in mo2[i].items():
+        if k in second_quartely:
+            second_quartely[k] += v
+        else:
+            second_quartely[k] = v
+print('第二季度')
+max_sale(second_quartely)
+third_quartely = {}
+mo3 = {}
+for i in [8, 9, 10]:
+    mo3[i] = mother_sale(i)
+    for k, v in mo3[i].items():
+        if k in third_quartely:
+            third_quartely[k] += v
+        else:
+            third_quartely[k] = v
+print('第三季度')
+max_sale(third_quartely)
+forth_quartely = {}
+mo4 = {}
+for i in [10, 11, 12]:
+    mo4[i] = mother_sale(i)
+    for k, v in mo4[i].items():
+        if k in forth_quartely:
+            forth_quartely[k] += v
+        else:
+            forth_quartely[k] = v
+print('第四季度')
+max_sale(forth_quartely)
+# 全年销量最低的衣服
+print('全年销售最少')
 min_sale(y)
